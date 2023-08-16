1260662870d147665e1a4df512532cb14344e446
Wesley Gurgel <wesleygurgel27@gmail.com>
2023-08-16 13:37:56 -0300

    feat: events

---
+++ src/events/message.js
@@ -0,0 +1,3 @@
+module.exports = (client, message) => {
+  console.log(`Mensagem recebida de ${message.author.tag}: ${message.content}`);
+};
---
+++ src/events/ready.js
@@ -0,0 +1,3 @@
+module.exports = client => {
+  console.log(`Bot logado como ${client.user.tag}!`);
+};
